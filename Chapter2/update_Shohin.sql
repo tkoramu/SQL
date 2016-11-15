c7dde220d9c43c80f3a8e76e44c294a6d6ec5234
tkoramu <Takashi-com@users.noreply.github.com>
2016-11-15 22:12:18 +0900

    update

--- Chapter2/update_Shohin.sql
+++ Chapter2/update_Shohin.sql
@@ -1,10 +1,10 @@
-UPDATE Shohin SET torokubi = '2009-09-20' WHERE id = '0001';
-UPDATE Shohin SET torokubi = '2009-09-11' WHERE id = '0002';
-UPDATE Shohin SET torokubi = NULL         WHERE id = '0003';
-UPDATE Shohin SET torokubi = '2009-09-20' WHERE id = '0004';
-UPDATE Shohin SET torokubi = '2009-01-15' WHERE id = '0005';
-UPDATE Shohin SET torokubi = '2009-09-20' WHERE id = '0006';
-UPDATE Shohin SET torokubi = '2008-04-28' WHERE id = '0007';
-UPDATE Shohin SET torokubi = '2009-11-11' WHERE id = '0008';
+UPDATE Shohin SET torokubi = '2009-09-20' WHERE shohin_id = '0001';
+UPDATE Shohin SET torokubi = '2009-09-11' WHERE shohin_id = '0002';
+UPDATE Shohin SET torokubi = NULL         WHERE shohin_id = '0003';
+UPDATE Shohin SET torokubi = '2009-09-20' WHERE shohin_id = '0004';
+UPDATE Shohin SET torokubi = '2009-01-15' WHERE shohin_id = '0005';
+UPDATE Shohin SET torokubi = '2009-09-20' WHERE shohin_id = '0006';
+UPDATE Shohin SET torokubi = '2008-04-28' WHERE shohin_id = '0007';
+UPDATE Shohin SET torokubi = '2009-11-11' WHERE shohin_id = '0008';
 
 COMMIT;
