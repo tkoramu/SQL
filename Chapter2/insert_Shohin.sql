1d3eb928f6f5f92ce8b1eac6bfb64050df155e24
tkoramu <Takashi-com@users.noreply.github.com>
2016-11-15 21:59:07 +0900

    create

---
+++ Chapter2/insert_Shohin.sql
@@ -0,0 +1,11 @@
+-- データを追加
+INSERT INTO Shohin VALUES ('0001', 'Tシャツ', '衣服', 1000, 500, '2009-09-11');
+INSERT INTO Shohin VALUES ('0002', '穴あけパンチ', '事務用品', 1000, 500, '2009-09-11');
+INSERT INTO Shohin VALUES ('0003', 'カッターシャツ', '衣服', 1000, 500, '2009-09-11');
+INSERT INTO Shohin VALUES ('0004', '包丁', 'キッチン用品', 1000, 500, '2009-09-11');
+INSERT INTO Shohin VALUES ('0005', '圧力鍋', 'キッチン用品', 1000, 500, '2009-09-11');
+INSERT INTO Shohin VALUES ('0006', 'フォーク', 'キッチン用品', 1000, 500, '2009-09-11');
+INSERT INTO Shohin VALUES ('0007', 'おろしがね', 'キッチン用品', 1000, 500, '2009-09-11');
+INSERT INTO Shohin VALUES ('0008', 'ボールペン', '事務用品', 1000, 500, '2009-09-11');
+
+COMMIT;
