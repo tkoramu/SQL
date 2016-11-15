7583a61d736563d686f295858b5b2d7fb55b48c0
tkoramu <Takashi-com@users.noreply.github.com>
2016-11-15 22:31:03 +0900

    create

---
+++ Chapter2/select_Shohin_as.sql
@@ -0,0 +1,11 @@
+SELECT
+    shohin_id   AS id,
+    shohin_mei  AS name,
+    shiire_tanka AS tanka
+FROM Shohin;
+
+SELECT
+    shohin_id   AS "商品ID",
+    shohin_mei  AS "商品名",
+    shiire_tanka AS "仕入単価"
+FROM Shohin;
