078a69074a402677a52e7d16b385d81b0390e43c
tkoramu <Takashi-com@users.noreply.github.com>
2016-11-15 22:10:29 +0900

    create

---
+++ Chapter2/update_Shohin.sql
@@ -0,0 +1,8 @@
+UPDATE Shohin SET torokubi = '2009-09-20' WHERE id = '0001';
+UPDATE Shohin SET torokubi = '2009-09-11' WHERE id = '0002';
+UPDATE Shohin SET torokubi = NULL         WHERE id = '0003';
+UPDATE Shohin SET torokubi = '2009-09-20' WHERE id = '0004';
+UPDATE Shohin SET torokubi = '2009-01-15' WHERE id = '0005';
+UPDATE Shohin SET torokubi = '2009-09-20' WHERE id = '0006';
+UPDATE Shohin SET torokubi = '2008-04-28' WHERE id = '0007';
+UPDATE Shohin SET torokubi = '2009-11-11' WHERE id = '0008';
