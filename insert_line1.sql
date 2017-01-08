47a6ba90db3c2c6e215e6b5b89fc0d209b256c8f
tkoramu <Takashi-com@users.noreply.github.com>
2017-01-08 18:45:51 +0900

    add INSERTについて

---
+++ insert_line1.sql
@@ -0,0 +1,16 @@
+INSERT INTO ShohinIns
+(
+    shohin_id
+    ,shohin_mei
+    ,shohin_bunrui
+    ,hanbai_tanka
+    ,shiire_tanka
+    ,torokubi
+) VALUES (
+    '0001'
+    ,'Tシャツ'
+    ,'衣服'
+    ,1000
+    ,500
+    ,'2009-09-20'
+);
---
+++ insert_null.sql
@@ -0,0 +1,9 @@
+-- NULLを割り当てれるのはNOT NULL制約がついていないもの
+INSERT INTO ShohinIns VALUES (
+    '0006'
+    ,'フォーク'
+    ,'キッチン用品'
+    ,500
+    ,NULL
+    ,'2009-09-20'
+);
