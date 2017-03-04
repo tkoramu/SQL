07b5397e7ced8b180bb8b085271cc2bd95ff8d44
tkoramu <Takashi-com@users.noreply.github.com>
2017-03-04 20:51:36 +0900

    update createAbleToView.sql 20170304 2051

--- createAbleToView.sql
+++ createAbleToView.sql
@@ -4,3 +4,9 @@ AS
 SELECT *
 FROM Shohin
 WHERE shohin_bunrui = '事務用品';
+
+-- ビューを使用して更新する
+INSERT INTO ShohinJim VALUES ('0009', '印鑑', '事務用品', 95, 10, '2009-11-30');
+
+-- ビューに追加されていることの確認
+SELECT * FROM ShohinJim;
